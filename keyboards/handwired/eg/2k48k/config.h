1226ec576d5fe57dab811cffafb6e213e1ac67e8
Ervin Gegprifti <gegprifti.ervin@gmail.com>
2025-01-23 10:58:02 +0100

    added rotary encoders

---
+++ keyboards/handwired/eg/2k48k/config.h
@@ -0,0 +1,22 @@
+/* Copyright 2024 Ervin Gegprifti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// https://docs.qmk.fm/features/encoders
+#define ENCODERS_PAD_A { D3, D1 }
+#define ENCODERS_PAD_B { D2, D0 }
+#define ENCODER_RESOLUTION 4
